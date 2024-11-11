108367a7bbab913e35b26ca770a3fac456fd5eff
Pietro Vischia <pietro.vischia@gmail.com>
2024-11-11 09:59:17 +0100

    More options

--- sergios_checkjobs.py
+++ sergios_checkjobs.py
@@ -4,7 +4,6 @@ import os
 
 production_paths=[
     "OutCmsBatch_25Oct24_141252",
-    #"frqcd_mc_2018_withpnet",
 ]
 
 remote_paths=[
@@ -12,20 +11,36 @@ remote_paths=[
     #"/eos/cms/store/cmst3/group/tthlep/sesanche/NanoTrees_forCMGRDF_100524_summerstudent_kistinjonas_mc/",
 ]
 from sys import argv
+import argparse
+parser = argparse.ArgumentParser(
+                    prog='Checkscript',
+                    description='Checks things',
+                    epilog='Always be awesome')
+parser.add_argument('-p', '--production_paths', type=list, default=["OutCmsBatch_25Oct24_141252"])
+parser.add_argument('-r','--remote_paths', type=list, default=["/eos/cms/store/group/phys_higgs/vischia/tth_run3/2024-10-25_test/"])
+parser.add_argument('-l', '--local', action='store_true', help="This is if you want to resubmit locally (correctly removes")
+parser.add_argument('-d', '--dryrun', action='store_true', help="Print paths and exit")
+parser.add_argument('-v', '--verbose', action='store_true')
+args = parser.parse_args()
 
-if len(argv)==3:
-    production_paths = [argv[1]]
-    remote_paths = [argv[2]]
+production_paths=args.production_paths
+remote_paths=args.remote_paths
 
 print("Production paths:", production_paths)
 print("Remote paths:", remote_paths)
+print("Giving local instructions:", args.local)
 
-
+if args.dryrun:
+    quit()
 def print_is_missing(file_name, production_path):
     #print(f'{file_name} missing')
     #print(f"Chunk {chunk} has failed") 
-    print(f"cd {production_path}/{chunk}; run_condor_simple.sh -t 480 ./batchScript.sh; cd -")
-
+    if args.local:
+        print(f"rm -r {production_path}/{chunk}/cache; rm -r {production_path}/{chunk}/job; cd {production_path}/{chunk}; setenv LS_SUBCWD $PWD/; ./batchScript.sh; cd -")
+    else:
+        print(f"cd {production_path}/{chunk}; run_condor_simple.sh -t 480 ./batchScript.sh; cd -")
+    
+ 
 for production_path, remote_path in zip( production_paths, remote_paths):
     for chunk in os.listdir(production_path):
         if not os.path.isdir(f'{production_path}/{chunk}'): continue
